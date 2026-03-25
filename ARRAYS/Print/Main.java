994026e8578d303692b683e8fa7d43bda246f7b6
UNDELA VASANTH REDDY <vasanthreddyreddy04@gmail.com>
2026-03-25 20:27:52 +0530

    Implement array printing in Main.java
    Fix array variable name and print elements.

---
+++ ARRAYS/Print/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] args = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
