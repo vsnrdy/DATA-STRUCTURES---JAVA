13b705bdc677773410b2f236e5f52e7e5ea86426
UNDELA VASANTH REDDY <vasanthreddyreddy04@gmail.com>
2026-03-25 20:50:41 +0530

    Rename Print class to PrintArray

--- ARRAYS/PrintArray.java
+++ ARRAYS/PrintArray.java
@@ -1,6 +1,6 @@
-public class Main {
+public class PrintArray {
     public static void main(String[] args) {
-        int[] args = {10, 20, 30, 40, 50};
+        int[] arr = {10, 20, 30, 40, 50};
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
