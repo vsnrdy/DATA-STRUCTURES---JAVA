791cb1bc5740fcad607557ad7fca8efd39b6f782
UNDELA VASANTH REDDY <vasanthreddyreddy04@gmail.com>
2026-04-24 23:13:41 +0530

    Update MaxElement.java

--- ARRAYS/MaxElement.java
+++ ARRAYS/MaxElement.java
@@ -2,7 +2,6 @@ public class MaxElement {
     public static void main(String[] args) {
 
         int[] arr = {10, 50, 30, 90, 20};
-
         int max = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
